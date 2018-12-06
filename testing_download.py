f8365f353037cdb7222b4e5ae96018ac34abb2e9
Carlos Alvarado <carlosalvarado1269@gmail.com>
2018-12-05 21:08:25 -0600

    testing download

--- testing_download.py
+++ testing_download.py
@@ -5,6 +5,6 @@ import subprocess
 os.system("curl https://bootstrap.pypa.io/get-pip.py > get-pip.py")
 os.system("sudo pip install pygame")
 dirname = os.path.dirname(os.path.abspath(__file__))
-cmd = os.path.join(dirname, 'snake.py')
-
-subprocess.call(cmd)
+cmd = os.path.join("cd ", dirname)
+#subprocess.call(cmd)
+os.system('python snake.py')
