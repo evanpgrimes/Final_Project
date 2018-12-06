d473684539d42e8f98487ccdbab5bd21ae60b140
Isai Cruz <icruz1996@yahoo.com>
2018-12-05 19:24:50 -0600

    ssa

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 # Learning Game
 
-import random, tkinter, tkinter.ttk, sys
+import random, tkinter, tkinter.ttk, sys, turtle, os
 
 
 class Player:
@@ -98,7 +98,7 @@ def level(currentLevel,player):
     
 
     newWindow = tkinter.Tk()
-    newWindow.geometry("550x300")
+    newWindow.geometry("3000x2000")
     newWindow.title("Begin Game")
 
     label1 = tkinter.Label(newWindow, text= "\n\n\tAre you ready to play level " + str(currentLevel) + ', ' + str(player.name) + " ?",font=("Times New Roman", 20))
@@ -113,7 +113,7 @@ def level(currentLevel,player):
     for i in range(0,10):
 
         qWindow = tkinter.Tk()
-        qWindow.geometry("700x700")
+        qWindow.geometry("2000x2000")
         qWindow.title("Question " + str(i+1))
 
         question, Correctanswer, answerChoices, idx =questions.askQuestion()
@@ -159,7 +159,7 @@ def level(currentLevel,player):
         else:
 
             ansWindow = tkinter.Tk()
-            ansWindow.geometry("700x700")
+            ansWindow.geometry("2000x2000")
             ansWindow.title("Result " + str(i+1))
 
             label1 = tkinter.Label(ansWindow, text= "\n\tYou are incorrect", font=("Times New Roman", 20))
@@ -199,13 +199,138 @@ def level_high_score(level, score):
         line += 1
 
 
+
+def pong_game():
+    wn = turtle.Screen()
+    wn.title("Pong")
+    wn.bgcolor("black")
+    wn.setup(width=800, height=600)
+    wn.tracer(0)
+
+    # Score
+    score_a = 0
+    score_b = 0
+
+    # Paddle A
+    paddle_a = turtle.Turtle()
+    paddle_a.speed(0)
+    paddle_a.shape("square")
+    paddle_a.color("white")
+    paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+    paddle_a.penup()
+    paddle_a.goto(-350, 0)
+
+    # Paddle B
+    paddle_b = turtle.Turtle()
+    paddle_b.speed(0)
+    paddle_b.shape("square")
+    paddle_b.color("white")
+    paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+    paddle_b.penup()
+    paddle_b.goto(350, 0)
+
+    # Ball
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("square")
+    ball.color("white")
+    ball.penup()
+    ball.goto(0, 0)
+    ball.dx = 2
+    ball.dy = 2
+
+    # Pen
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.shape("square")
+    pen.color("white")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 260)
+    pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+    # Functions
+    def paddle_a_up():
+        y = paddle_a.ycor()
+        y += 20
+        paddle_a.sety(y)
+
+    def paddle_a_down():
+        y = paddle_a.ycor()
+        y -= 20
+        paddle_a.sety(y)
+
+    def paddle_b_up():
+        y = paddle_b.ycor()
+        y += 20
+        paddle_b.sety(y)
+
+    def paddle_b_down():
+        y = paddle_b.ycor()
+        y -= 20
+        paddle_b.sety(y)
+
+    # Keyboard bindings
+    wn.listen()
+    wn.onkeypress(paddle_a_up, "w")
+    wn.onkeypress(paddle_a_down, "s")
+    wn.onkeypress(paddle_b_up, "Up")
+    wn.onkeypress(paddle_b_down, "Down")
+
+    # Main game loop
+    while True:
+        wn.update()
+
+        # Move the ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # Border checking
+
+        # Top and bottom
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ball.dy *= -1
+            os.system("afplay bounce.wav&")
+
+        elif ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1
+            os.system("afplay bounce.wav&")
+
+        # Left and right
+        if ball.xcor() > 350:
+            score_a += 1
+            pen.clear()
+            pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+
+        elif ball.xcor() < -350:
+            score_b += 1
+            pen.clear()
+            pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+            ball.goto(0, 0)
+            ball.dx *= -1
+
+        # Paddle and ball collisions
+        if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+            ball.dx *= -1
+            os.system("afplay bounce.wav&")
+
+        elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+            ball.dx *= -1
+            os.system("afplay bounce.wav&")
+
+
 def main():
+
 
     def onClickCombo():
         label.configure(text="input: " + combo.get())
 
     window = tkinter.Tk()
-    window.geometry("700x700")
+    window.geometry("2000x2000")
     window.title("Learning is Fun!")
 
     # Switch frames
@@ -260,7 +385,7 @@ def main():
     while keepPlaying:
         if Student.score >=7.0:
             Scorewindow = tkinter.Tk()
-            Scorewindow.geometry("700x700")
+            Scorewindow.geometry("2000x2000")
             Scorewindow.title("level " + str(currentLevel) + " Score")
 
             label1 = tkinter.Label(Scorewindow, text="\tScore:\t" + str(Student.score), font=("Times New Roman", 20))
@@ -280,7 +405,7 @@ def main():
         else:
 
             Scorewindow = tkinter.Tk()
-            Scorewindow.geometry("700x700")
+            Scorewindow.geometry("2000x2000")
             Scorewindow.title("level " + str(currentLevel) + " Score")
 
             label1 = tkinter.Label(Scorewindow, text="\tScore:\t" + str(Student.score), font=("Times New Roman", 20))
@@ -307,6 +432,7 @@ def main():
             else:
                 keepPlaying = False
                 sys.exit()
+    
 
 
 main()
